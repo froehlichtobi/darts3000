6a79c72e9019a3674572e29a13d953881dc38346
Tobias Fröhlich <90284111+froehlichtobi@users.noreply.github.com>
2024-08-04 12:36:03 +0000

    mehr css anpassungen

--- darts3000/src/Players.js
+++ darts3000/src/Players.js
@@ -5,16 +5,33 @@ const Players = ({ addPlayer, removePlayer, players }) => {
     <div className="dartsPlayers">
       <h2>Players</h2>
       {players.map((player, index) => (
-        <div key={index}>
+        <div 
+        key={index}
+        className="connectInputAndButton"
+        >
           <input
             type="text"
             value={player}
             onChange={(e) => addPlayer(e.target.value, index)}
           />
-          <button onClick={() => removePlayer(index)}>Remove</button>
+          <button 
+          onClick={() => removePlayer(index)}
+          style={{
+            borderTopRightRadius: '4px',
+            borderBottomRightRadius: '4px',
+            marginLeft: '0px',
+          }}
+          >
+            X</button>
         </div>
       ))}
-      <button onClick={() => addPlayer("", players.length)}>Add Player</button>
+      <button 
+        onClick={() => addPlayer("", players.length)}
+        style={{
+          borderRadius: '4px',
+        }}
+        >
+        Add Player</button>
     </div>
   );
 };
