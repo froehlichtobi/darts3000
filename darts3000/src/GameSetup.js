cc0785b7771e7025649c64db6c2a788e3dd1fa2e
Tobias Fröhlich <90284111+froehlichtobi@users.noreply.github.com>
2024-08-05 12:28:32 +0000

    added margin between players (in input and button so that they are aligned)

---
+++ darts3000/src/GameSetup.js
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+
+// maybe create a big object (?) with all the details and variables needed for a game (startingscore, finishingmode, players, time, gameWon, Date and Time, scores,...)
