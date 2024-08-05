3a4af77a59d21a6ef95204f56bc7aad8d5f9c52f
Tobias Fröhlich <90284111+froehlichtobi@users.noreply.github.com>
2024-08-05 13:12:23 +0000

    not much

--- darts3000/src/GameSetup.js
+++ darts3000/src/GameSetup.js
@@ -1,3 +1,6 @@
 import React, { useState } from 'react';
 
 // maybe create a big object (?) with all the details and variables needed for a game (startingscore, finishingmode, players, time, gameWon, Date and Time, scores,...)
+//   const handleNextTurn = () => {
+//    const nextPlayerIndex = (currentPlayerIndex + 1) % players.length;
+//   setCurrentPlayerIndex(nextPlayerIndex);
